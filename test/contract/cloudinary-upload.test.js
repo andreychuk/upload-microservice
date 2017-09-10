de15ccbeeff5096a672f9713b986ee085ed4c976
Dmitry Velichko <dmitry.velichko@gmail.com>
2017-09-10 21:53:46 +0300

    WIP: start workingon automated tests

--- test/contract/cloudinary-upload.test.js
+++ test/contract/cloudinary-upload.test.js
@@ -1,13 +1,16 @@
 const { test, superTest } = require('../test-env');
 
 test('test upload file cloudinary', async (t) => {
-  const { statusCode } = await superTest.post('/cl/upload').attach('input', '/tmp/1.jpg').send();
+  const { statusCode } = await superTest.post('/cl/upload')
+    .attach('input', '/tmp/1.jpg')
+    .send();
   t.is(statusCode, 201);
 });
 
 test('test upload files cloudinary', async (t) => {
   const { statusCode } = await superTest.post('/cl/upload')
     .attach('input', '/tmp/2.jpg')
-    .attach('input', '/tmp/3.jpg').send();
+    .attach('input', '/tmp/3.jpg')
+    .send();
   t.is(statusCode, 201);
 });

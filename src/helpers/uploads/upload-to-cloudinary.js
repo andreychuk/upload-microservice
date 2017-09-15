c392940e7c1fa3c6c72b7aa390707f46df751309
Dmitry Velichko <dmitry.velichko@gmail.com>
2017-09-15 11:59:46 +0300

    WIP: added cloudinary client configuration

--- src/helpers/uploads/upload-to-cloudinary.js
+++ src/helpers/uploads/upload-to-cloudinary.js
@@ -2,14 +2,20 @@ const ClodinaryClient = require('cloudinary').v2;
 const _ = require('lodash');
 const Promise = require('bluebird');
 const httpError = require('http-errors');
+const config = require('smart-config').get('cloudinary');
 
 module.exports = async ({ input }) => {
+  ClodinaryClient.config({
+    cloud_name: config.cloud_name,
+    api_key: config.api_key,
+    api_secret: config.api_secret
+  });
   return uploadMany(ClodinaryClient, input);
 };
 
 function upload(client, file) {
   return new Promise((resolve, reject) => {
-    client.upload(file, (error, result) => {
+    client.uploader.upload(file, (error, result) => {
       if (error) return reject(httpError(error.statusCode, error.message));
       resolve({ key: result.public_id, url: result.url });
     });

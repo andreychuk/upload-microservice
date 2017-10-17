12de792f4b38a34cd391cc99dc8f660d96a0ee1a
dmitry <dmitry.velichko@artmyweb.com>
2017-10-17 17:31:25 +0300

    WIP: working on unit-testing, added dummy files for upload testing

--- test/contract/cloudinary-upload.test.js
+++ test/contract/cloudinary-upload.test.js
@@ -1,16 +1,17 @@
 const { test, superTest } = require('../test-env');
+const path = require('path');
 
 test('test upload file cloudinary', async (t) => {
-  const { statusCode } = await superTest.post('/cl/upload')
-    .attach('input', '/tmp/1.jpg')
+  const { statusCode } = await superTest.post('/cloudinary/upload')
+    .attach('input', path.join(__dirname, '..', '/tmp/1.jpg'))
     .send();
   t.is(statusCode, 201);
 });
 
 test('test upload files cloudinary', async (t) => {
-  const { statusCode } = await superTest.post('/cl/upload')
-    .attach('input', '/tmp/2.jpg')
-    .attach('input', '/tmp/3.jpg')
+  const { statusCode } = await superTest.post('/cloudinary/upload')
+    .attach('input', path.join(__dirname, '..', '/tmp/2.jpg'))
+    .attach('input', path.join(__dirname, '..', '/tmp/3.jpg'))
     .send();
   t.is(statusCode, 201);
 });
--- test/contract/s3-upload.test.js
+++ test/contract/s3-upload.test.js
@@ -1,16 +1,17 @@
 const { test, superTest } = require('../test-env');
+const path = require('path');
 
 test('test upload file', async (t) => {
   const { statusCode } = await superTest.post('/s3/upload')
-    .attach('input', '/tmp/1.jpg')
+    .attach('input', path.join(__dirname, '..', '/tmp/1.jpg'))
     .send();
   t.is(statusCode, 201);
 });
 
 test('test upload files', async (t) => {
   const { statusCode } = await superTest.post('/s3/upload')
-    .attach('input', '/tmp/2.jpg')
-    .attach('input', '/tmp/3.jpg')
+    .attach('input', path.join(__dirname, '..', '/tmp/2.jpg'))
+    .attach('input', path.join(__dirname, '..', '/tmp/3.jpg'))
     .send();
   t.is(statusCode, 201);
 });

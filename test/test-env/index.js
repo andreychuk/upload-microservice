f06f1546b593262c54caf1b0fc592a61cbcfffa7
dmitry <dmitry.velichko@artmyweb.com>
2017-10-19 21:38:19 +0300

    WIP: added dummy test to avoid uncaught exception error on auto-test

--- test/test-env/index.js
+++ test/test-env/index.js
@@ -4,4 +4,9 @@ const app = require('../../src/app');
 const superTest = require('supertest-promised')(app);
 const test = require('ava');
 
+
+test('dummy test', (t) => {
+  t.is(true, true);
+});
+
 module.exports = { superTest, app, test };

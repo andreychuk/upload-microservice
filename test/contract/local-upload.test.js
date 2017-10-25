d1072592a802b275ade499850a4b8eff9a167eb1
dmitry <dmitry.velichko@artmyweb.com>
2017-10-25 21:31:51 +0300

    bugfix: fixed formatting

--- test/contract/local-upload.test.js
+++ test/contract/local-upload.test.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 test('test upload file local', async (t) => {
   const resp = await superTest.post('/local/upload')
-  .attach('input', path.join(__dirname, '..', '/tmp/1.jpg'));
+    .attach('input', path.join(__dirname, '..', '/tmp/1.jpg'));
 
   t.is(resp.statusCode, 201);
 });
 
 test('test upload files local', async (t) => {
   const resp = await superTest.post('/local/upload')
-  .attach('input', path.join(__dirname, '..', '/tmp/2.jpg'))
-  .attach('input', path.join(__dirname, '..', '/tmp/3.jpg'));
+    .attach('input', path.join(__dirname, '..', '/tmp/2.jpg'))
+    .attach('input', path.join(__dirname, '..', '/tmp/3.jpg'));
 
   t.is(resp.statusCode, 201);
 });

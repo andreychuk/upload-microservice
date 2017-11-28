bc2ff28593c56b7ac75adef204b2976529f50b75
velichkod <dmitry.velichko@gmail.com>
2017-11-28 17:44:03 +0200

    WIP: implementing JWT token support

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,10 @@
 const { test, superTest } = require('./test-env/_test_env');
+const jwtSecret = require('smart-config').get('JWTSecret');
+const jwt = require('jsonwebtoken');
 
 test('test root route', async (t) => {
-  const { statusCode } = await superTest.get('/');
+  const ValidToken = jwt.sign({}, jwtSecret);
+
+  const { statusCode } = await superTest.get('/').set('Authorization', ValidToken);
   t.is(statusCode, 200);
 });

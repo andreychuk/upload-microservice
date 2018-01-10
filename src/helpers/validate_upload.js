17520209eb5be291ea0c744e742ebd1595578336
velichkod <dmitry.velichko@gmail.com>
2018-01-10 14:33:37 +0200

    upload file MIME-type validation - implemented simplified version

--- src/helpers/validate_upload.js
+++ src/helpers/validate_upload.js
@@ -6,11 +6,11 @@ module.exports = (file) => {
     if (uploadMimes === "" || uploadMimes === "UPLOAD_MIME_TYPES") {
       resolve(true);
     }
-    if (file.mimeType === "") {
+    if (file.mimetype === "") {
       reject(new Error('Unable to detect MIME type'));
     }
 
-    if (mimesList.indexOf(file.mimeType) === -1) {
+    if (mimesList.indexOf(file.mimetype) === -1) {
       return reject(new Error('MIME type not allowed'));
     }
     return resolve(true);

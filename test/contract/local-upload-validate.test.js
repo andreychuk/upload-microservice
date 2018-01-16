72768a7b2fd796012a41b7e59730cd4ac19a7832
velichkod <dmitry.velichko@gmail.com>
2018-01-16 16:25:30 +0200

    bugfix: fixed the test to properly handle case when no mime-types defined for validation

--- test/contract/local-upload-validate.test.js
+++ test/contract/local-upload-validate.test.js
@@ -2,6 +2,7 @@ const { test, superTest } = require('../test-env/_test_env');
 const path = require('path');
 const jwtSecret = require('smart-config').get('JWTSecret');
 const jwt = require('jsonwebtoken');
+const uploadMimes = require('smart-config').get('uploadMimes');
 
 test.beforeEach('preparing jwt token', (t) => {
   t.context = {};
@@ -21,5 +22,9 @@ test('test validate single text file', async (t) => {
     .attach('input', path.join(__dirname, '..', '/tmp/1.txt'))
     .set('Authorization', t.context.token);
 
-  t.is(resp.statusCode, 400);
+  if (uploadMimes === 'UPLOAD_MIME_TYPES' || uploadMimes === '') {
+    t.is(resp.statusCode, 201);
+  } else {
+    t.is(resp.statusCode, 400);
+  }
 });

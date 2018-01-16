b81a11b9f1d5fb13dd355d17322377e425908011
velichkod <dmitry.velichko@gmail.com>
2018-01-16 16:38:02 +0200

    refactoring - reducing code complexity

--- src/helpers/validate_upload.js
+++ src/helpers/validate_upload.js
@@ -6,17 +6,27 @@ module.exports = (file) => {
 
 const fileValidate = (file) => {
   return new Promise((resolve, reject) => {
-    const mimesList = uploadMimes.split(" ");
-    if (uploadMimes === "" || uploadMimes === "UPLOAD_MIME_TYPES") {
-      resolve(true);
+    const mimesList = fileValidateMimes();
+
+    if (mimesList === false) {
+      return resolve(true);
     }
+
     if (file.mimetype === "") {
-      reject(new Error('Unable to detect MIME type'));
+      return reject(new Error('Unable to detect MIME type'));
     }
 
     if (mimesList.indexOf(file.mimetype) === -1) {
       return reject(new Error('MIME type not allowed'));
     }
+
     return resolve(true);
   });
+};
+
+const fileValidateMimes = () => {
+  if (uploadMimes === "" || uploadMimes === "UPLOAD_MIME_TYPES") {
+    return false;
+  }
+  return uploadMimes.split(" ");
 };

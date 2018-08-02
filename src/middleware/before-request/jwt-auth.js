8f8cb5b667b28bc31856a7663af6db0870ed9595
vanya andreychuk <vandreychyk@gmail.com>
2018-08-02 18:18:06 +0300

    - fix typo

--- src/middleware/before-request/jwt-auth.js
+++ src/middleware/before-request/jwt-auth.js
@@ -4,20 +4,21 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
 
-  if (jwtDisable === true) return next();
+  if (jwtDisable !== "true") {
 
-  if (typeof req.headers.authorization !== "string") {
-    return res.status(400).send("Bad Request");
-  }
+    if (typeof req.headers.authorization !== "string") {
+      return res.status(400).send("Bad Request");
+    }
 
-  if (req.headers.authorization.indexOf('Bearer ') === -1) {
-    return res.status(400).send("Bad Request");
-  }
+    if (req.headers.authorization.indexOf('Bearer ') === -1) {
+      return res.status(400).send("Bad Request");
+    }
 
-  try {
-    jwt.verify(req.headers.authorization.replace('Bearer ', ''), jwtSecret);
-  } catch (err) {
-    return res.status(401).send('Unauthorized');
+    try {
+      jwt.verify(req.headers.authorization.replace('Bearer ', ''), jwtSecret);
+    } catch (err) {
+      return res.status(401).send('Unauthorized');
+    }
   }
 
   return next();

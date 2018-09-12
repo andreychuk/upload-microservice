473c0db91217d0b6590131e35494d3d707b57d52
vanya andreychuk <vandreychyk@gmail.com>
2018-09-12 14:10:42 +0300

    Fix: Set ContentDisposition inline

--- src/middleware/file-download.js
+++ src/middleware/file-download.js
@@ -1,6 +1,7 @@
 
 module.exports = (req, res, next) => {
   if (typeof res.data.fileName !== 'undefined' && typeof res.data.filePath !== 'undefined') {
+    res.header = Object.assign(res.header, { ContentDisposition: "inline; filename=" + res.data.fileName });
     return res.download(res.data.filePath);
   }
   next();

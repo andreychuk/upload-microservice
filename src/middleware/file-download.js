fe6c8866a64508b0a530c3a1d106be684367f518
Maru-cat <belolutskaya@gmail.com>
2018-09-13 21:29:02 +0300

    - change download image to display on local/get/id url

--- src/middleware/file-download.js
+++ src/middleware/file-download.js
@@ -1,8 +1,8 @@
+var path = require('path');
 
 module.exports = (req, res, next) => {
   if (typeof res.data.fileName !== 'undefined' && typeof res.data.filePath !== 'undefined') {
-    res.header = Object.assign(res.header, { ContentDisposition: "inline; filename=" + res.data.fileName });
-    return res.download(res.data.filePath);
+    return res.sendFile(path.resolve(res.data.filePath));
   }
   next();
 };

95ae00c6e43e60bcd51f241c000481a1949298cc
vanya andreychuk <vandreychyk@gmail.com>
2018-09-18 15:41:27 +0300

    - fix auth complexity

--- src/middleware/before-request/jwt-auth.js
+++ src/middleware/before-request/jwt-auth.js
@@ -5,17 +5,22 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
 
   if (jwtDisable !== "true") {
-
-    if ((typeof req.headers.authorization !== "string") || (req.headers.authorization.indexOf('Bearer ') === -1)) {
-      return res.status(400).send("Bad Request");
-    }
-
-    try {
-      jwt.verify(req.headers.authorization.replace('Bearer ', ''), jwtSecret);
-    } catch (err) {
-      return res.status(401).send('Unauthorized');
-    }
+    if (checkAuth(req, res) !== true) return checkAuth(req, res);
   }
 
   return next();
 };
+
+function checkAuth(req, res) {
+  if ((typeof req.headers.authorization !== "string") || (req.headers.authorization.indexOf('Bearer ') === -1)) {
+    return res.status(400).send("Bad Request");
+  }
+
+  try {
+
+    jwt.verify(req.headers.authorization.replace('Bearer ', ''), jwtSecret);
+  } catch (err) {
+    return res.status(401).send('Unauthorized');
+  }
+  return true;
+}
